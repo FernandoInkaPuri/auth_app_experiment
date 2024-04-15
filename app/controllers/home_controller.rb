4c085cc08293e66f0a91cc0e83ca040e7289842e
Fernando Puri <luiz.gomez@rebase.com.br>
2024-04-15 18:10:35 -0300

    auth app makes request to graphql api and receives policies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,23 @@
+require 'net/http'
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # Listar Policies
+    uri = URI('http://graphql_api:3001/graphql')
+    body = {
+      query: "query {
+        policies {
+          dataEmissao
+          segurado {
+            nome
+            cpf
+          }
+        }
+      }"
+    }
+    headers = { 'Content-Type' => 'application/json' }
+
+    response = Net::HTTP.post(uri, body.to_json, headers)
+    @policies = JSON.parse(response.body)
   end
 end
