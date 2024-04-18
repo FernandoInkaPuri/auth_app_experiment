d7edb69d2d2c986c179d68e8394053eee815da34
FernandoInkaPuri <luizfegomez@gmail.com>
2024-04-18 14:56:09 -0300

    Auth App Receives Policies From GraphQL Api
    * auth app makes request to graphql api and receives policies
    
    * adds error handling and message if there are no policies
    
    * improves the error message
    
    * Improves handling and error messages and fix a variable name
    
    ---------
    
    Co-authored-by: Fernando Puri <luiz.gomez@rebase.com.br>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,33 @@
+require 'net/http'
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # Listar Policies
+    uri = URI('http://graphql_api:3001/graphql')
+    body = {
+      query: "query {
+        policies {
+          dataEmissao
+          segurado {
+            nome
+            cpf
+          }
+        }
+      }"
+    }
+    headers = { 'Content-Type' => 'application/json' }
+
+    begin
+      response = Net::HTTP.post(uri, body.to_json, headers)
+      if response.code == "200"
+        @policies = JSON.parse(response.body)
+        @request_completed = true
+      else
+        raise "#{response.message}"
+      end
+    rescue StandardError => error
+      @error_message = "Ocorreu um erro e não foi possível fazer a consulta das apólices. Erro: #{error.message}"
+      Rails.logger.warn(@error_message)
+    end
   end
 end
